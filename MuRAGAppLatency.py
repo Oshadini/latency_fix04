e0094c51062d945feaf42efda9d6cb475ccaf76e
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-07 17:52:48 +0530

    Update MuRAGAppLatency.py

--- MuRAGAppLatency.py
+++ MuRAGAppLatency.py
@@ -214,7 +214,7 @@ if uploaded_file is not None:
             return base64.b64encode(image_file.read()).decode("utf-8")
     st.cache_data
     def image_summarize(img_base64, prompt):
-      """Make image summary"""
+        """Make image summary"""
         if immage_sum_model == 'gpt-4-vision-preview':
             model = ChatOpenAI(temperature=0, model=immage_sum_model, openai_api_key = openai.api_key, max_tokens=1024)
         else:
